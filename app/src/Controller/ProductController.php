f0212a5bcc203ec3519865468c17f2996cf26349
Ndiaye D <n.diabira@hotmail.fr>
2022-06-27 17:33:40 +0200

    add category in productController

--- app/src/Controller/ProductController.php
+++ app/src/Controller/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,9 +12,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 #[Route('/product', name: 'app_product_')]
 class ProductController extends AbstractController
 {
-    public function __construct(ProductRepository $productRepository)
+    public function __construct(ProductRepository $productRepository, CategoryRepository $categoryRepository)
     {
         $this->productRepository = $productRepository;
+        $this->categoryRepository = $categoryRepository;
     }
     
     #[Route(name: 'index')]
